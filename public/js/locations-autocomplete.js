c29b68edd5c4da17ec4a546226fabc80d5d23580
m-app <xlopez3000@gmail.com>
2017-11-27 12:33:59 -0600

    version funcional del sitio

---
+++ public/js/locations-autocomplete.js
@@ -0,0 +1,5 @@
+var locations = [
+{ value: "Cabecera - Guatemala", latLng: [14.6278762, -90.5108941]},
+{ value: "Villa Nueva - Guatemala", latLng: [14.528858, -90.591844]},
+{ value: "Mixco - Guatemala", latLng: [14.639777, -90.600674]},
+];
